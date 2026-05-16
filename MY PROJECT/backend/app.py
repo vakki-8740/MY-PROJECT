5bd10ad6e312ccce078fd206f17d802dea7e13ef
vakki-8740 <lightsir8740@gmail.com>
2026-05-17 00:12:22 +0530

    Update app.py

--- MY PROJECT/backend/app.py
+++ MY PROJECT/backend/app.py
@@ -1,3 +1,5 @@
+# backend/app.py
+
 from flask import Flask, send_from_directory
 from config import Config
 from routes import api
@@ -8,20 +10,24 @@ def create_app():
     app = Flask(__name__, static_folder='../frontend', static_url_path='')
     app.config.from_object(Config)
     
+    # Initialize DB
     init_db()
+    
+    # Register Blueprint
     app.register_blueprint(api, url_prefix='/api')
 
+    # Serve Frontend Files
     @app.route('/user/')
-    def serve_user(): 
+    def serve_user():
         return send_from_directory(os.path.join(app.root_path, '../frontend/user'), 'index.html')
     
     @app.route('/admin/')
-    def serve_admin(): 
+    def serve_admin():
         return send_from_directory(os.path.join(app.root_path, '../frontend/admin'), 'index.html')
 
     return app
 
-# Ye line zaroori hai for Gunicorn
+# YE LINE SABSE ZARURI HAI FOR GUNICORN
 app = create_app()
 
 if __name__ == '__main__':
